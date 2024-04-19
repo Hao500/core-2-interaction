2274fd96cd09a849fe209b91f67ad1424d28c84f
HaoZ2022 <115562388+HaoZ2022@users.noreply.github.com>
2024-04-18 22:51:43 -0400

    extension

---
+++ studio/extension/footer.js
@@ -0,0 +1,7 @@
+let everything=document.querySelectorAll("*");
+
+for (let thing of everything){
+    thing.classList.add("font-change");
+    thing.src="https://upload.wikimedia.org/wikipedia/commons/e/e2/2020-04-07_Prunus_%C3%97_yedoensis_Tambasasayama%2CHyogo%28%E4%B8%B9%E6%B3%A2%E7%AF%A0%E5%B1%B1%E5%B8%82%E7%AF%A0%E5%B1%B1%E5%B7%9D%E3%81%AE%E3%82%BD%E3%83%A1%E3%82%A4%E3%83%A8%E3%82%B7%E3%83%8E%29DSCF2986%E2%98%86%E5%BD%A1.jpg";
+    thing.srcset="https://upload.wikimedia.org/wikipedia/commons/e/e2/2020-04-07_Prunus_%C3%97_yedoensis_Tambasasayama%2CHyogo%28%E4%B8%B9%E6%B3%A2%E7%AF%A0%E5%B1%B1%E5%B8%82%E7%AF%A0%E5%B1%B1%E5%B7%9D%E3%81%AE%E3%82%BD%E3%83%A1%E3%82%A4%E3%83%A8%E3%82%B7%E3%83%8E%29DSCF2986%E2%98%86%E5%BD%A1.jpg";
+}
